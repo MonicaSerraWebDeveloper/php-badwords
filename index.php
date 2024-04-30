088cc44a685b6350655fb23f663fc01f39e6319e
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-30 14:32:35 +0200

    feat: add the form in index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-ciao
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP badwords</title>
+</head>
+<body>
+    <form action="">
+        <textarea name="paragraph" id="" cols="50" rows="20"></textarea>
+        <div>
+            <button>Invia il paragrafo</button>
+        </div>
+    </form>
+</body>
+</html>
